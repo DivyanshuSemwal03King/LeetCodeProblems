443aac706fc020d7d720f663cfde6edfbcc02008
DivyanshuSemwal03King <divyanshusemwal03king@gmail.com>
2026-03-03 16:02:49 +0530

    Merge Sorted Array

---
+++ LeetCode Problems/MergeSortedArray_88.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        
+    
+        sort(nums1.begin(), nums1.end());
+    }
+
+    int main(){
+        vector<int> nums1={1,2,3,0,0,0};
+        int m = nums1.size();
+        vector<int> nums2={2,5,6};
+        int n = nums2.size();
+        merge(nums1,m,nums2,n);
+    return 0;
+         
+    }
